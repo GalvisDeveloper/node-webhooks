c7cf4699a0bb26830dfb23f3562297577131f82c
GalvisDev <galvisdeveloper@gmail.com>
2024-09-17 22:54:20 -0500

    Actualizacion de importaciones archivos barril

--- src/presentation/index.ts
+++ src/presentation/index.ts
@@ -1,2 +1,5 @@
+export * from './index';
+export * from './modules/discord/discord.service';
 export * from './modules/github/github.controller';
-export * from './modules/github/github.routes';
+export * from './modules/github/github.service';
+export * from './modules';
